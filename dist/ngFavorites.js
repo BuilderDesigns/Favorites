83a20c5bddac8aaa38a0c0dcf9168158d1dea25e
Josh Willis <jwillis@builderdesigns.com>
2015-03-30 15:25:09 -0500

    Compiled code

--- dist/ngFavorites.js
+++ dist/ngFavorites.js
@@ -3,6 +3,7 @@ angular.module('Favorites', ['ui.bootstrap', 'Favorites.templates','angular.filt
         EVENTS:{
             FAV_ADDED:          "favoriteAdded",
             FAV_REMOVED:        "favoriteRemoved",
+            FAV_CHANGED:        "favoritesChanged",
             DASHBOARD_OPENED:   "dashboardOpened",
             DASHBOARD_CANCELED: "dashboardCanceled",
             DASHBOARD_OK:       "dashboardOk",
@@ -46,7 +47,7 @@ angular.module('Favorites', ['ui.bootstrap', 'Favorites.templates','angular.filt
         $rootScope.$watch('favorites', function(){
 
             MyFavorites.sync();
-
+            $rootScope.$emit(FavConfig.EVENTS.FAV_CHANGED,$rootScope.favorites);
             $rootScope.updateLinks();
 
         }, true);
@@ -122,7 +123,6 @@ angular.module('Favorites').controller(
 
         compilePdf('dataurlnewwindow');
 
-
     };
 
 
@@ -135,10 +135,6 @@ angular.module('Favorites').controller(
 
     function compilePdf(outputOption)
     {
-
-
-
-
 
     }
 
